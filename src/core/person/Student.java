6130492fc7b60beb53f6ed1c33ed7739532b4641
moritz <moritz@0x1lap.fritz.box>
2017-01-04 01:17:44 +0100

    new updates in the teacher and student class

--- src/core/person/Student.java
+++ src/core/person/Student.java
@@ -3,4 +3,10 @@ import core.form.Form;
 
 public class Student extends GeneralPerson {
 private Form mainform;
+public Student(int age, java.text.Normalizer.Form[] hasforms) {
+		super(age, hasforms);
+		// TODO Auto-generated constructor stub
+	}
+
+
 }
--- src/core/person/Teacher.java
+++ src/core/person/Teacher.java
@@ -1,4 +1,9 @@
 package core.person;
 import core.form.Form;
 public class Teacher extends GeneralPerson {
+
+	public Teacher(int age, java.text.Normalizer.Form[] hasforms) {
+		super(age, hasforms);
+		// TODO Auto-generated constructor stub
+	}
 }
